f9b22b095fd13dd780912150dededad59cba080b
Rais Salman Wibowo <rais.salman.wibowo.rsw@gmail.com>
2026-09-24 09:27:12 +0700

    Nambah VB nya

--- VS Code Ngasal.vb
+++ VS Code Ngasal.vb
@@ -2,10 +2,17 @@ Module Program
     Sub Start_Program()
         Dim A As Integer = 10
         Dim B As String = ""
+        Dim AA As String = ""
 
         If A == 10 Then
             B = "Hello There!"
         ElseIf A > 10 Then
-            B =
+            B = "Hey There!"
+        ElseIf A < 10 Then
+            B = "Halo{A}"
+        End If
+
+        Console.WriteLine(B)
+        AA = Console.ReadLine()
     End Sub
 End Module
