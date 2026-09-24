9c30b580abc5bb8881811f5287d62f19cb251693
Rais Salman Wibowo <rais.salman.wibowo.rsw@gmail.com>
2026-09-24 08:18:58 +0700

    Markazz Ambatron

--- Java Script ILK.js
+++ Java Script ILK.js
@@ -7,3 +7,15 @@ function A(){
         B = "Hello everyone!"
     }
 }
+function UnknownA1(){
+    let A = 0; let E = "";
+    if (A === 0){
+        E="Hello!"
+    } else if(A > 0){
+        E="Hello"
+    } else if(A < 0 && A > -10){
+        E="Hey There!"
+    } else{
+        E="Hey There"
+    }
+}
