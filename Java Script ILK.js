e2f16027c38fc6f62b15c11d0d1a5e6c829fa80b
Rais Salman Wibowo <rais.salman.wibowo.rsw@gmail.com>
2026-09-24 09:04:23 +0700

    Hello!

--- Java Script ILK.js
+++ Java Script ILK.js
@@ -2,20 +2,29 @@ function A(){
     let A = 10;
     let B = "";
     if (A === 10){
-        B = "Hello everyone"
+        B = "Hello everyone";
     }else{
-        B = "Hello everyone!"
+        B = "Hello everyone!";
     }
 }
 function UnknownA1(){
     let A = 0; let E = "";
     if (A === 0){
-        E="Hello!"
+        E="Hello!";
     } else if(A > 0){
-        E="Hello"
+        E="Hello";
     } else if(A < 0 && A > -10){
-        E="Hey There!"
+        E="Hey There!";
     } else{
-        E="Hey There"
+        E="Hey There";
     }
+}
+function Test1(){
+    let T = "";
+    const E = [["A", "B"],["C", "D"],['E', 'F']];
+    var J = 10;
+    if (J === 10 || J === 11){
+
+    }
+    console.writeline(E[1[1]] && T);
 }
---
+++ VS Code Ngasal.vb
@@ -0,0 +1,11 @@
+Module Program
+    Sub Start_Program()
+        Dim A As Integer = 10
+        Dim B As String = ""
+
+        If A == 10 Then
+            B = "Hello There!"
+        ElseIf A > 10 Then
+            B =
+    End Sub
+End Module
